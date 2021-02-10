82f74d86d5774ba182f0b2013f6e3889a38e2a8f
akirahasten <70085834+akirahasten@users.noreply.github.com>
2021-02-10 12:28:42 +0800

    運勢欠佳

---
+++ src/pages/cart/CartStep.js
@@ -0,0 +1,34 @@
+function CartStep(props) {
+  const { cartStep, setCartStep } = props
+  return (
+    <div className="row hhyy-step mt-5">
+      <div className="col hy-cart-step d-flex justify-content-between">
+        <div
+          className={'text-center ' + cartStep}
+          onClick={() => {
+            setCartStep('hy-step-active ')
+          }}
+        >
+          <h5>Step.1</h5>
+          <h5>購物清單</h5>
+        </div>
+        <div className="d-flex flex-column justify-content-center">
+          <i className="fas fa-chevron-right"></i>
+        </div>
+        <div className="text-center">
+          <h5>Step.2</h5>
+          <h5>配送與付款資訊</h5>
+        </div>
+        <div className="d-flex flex-column justify-content-center">
+          <i className="fas fa-chevron-right"></i>
+        </div>
+        <div className="text-center">
+          <h5>Step.3</h5>
+          <h5>感謝訂購</h5>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CartStep
