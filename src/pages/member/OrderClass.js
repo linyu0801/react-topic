992d36e76adf810a52af32864e67a63983d38794
fish <spe9g0908@gmail.com>
2021-02-12 20:35:42 +0800

    c

--- src/pages/member/OrderClass.js
+++ src/pages/member/OrderClass.js
@@ -2,164 +2,164 @@ import '../../styles/fish.scss'
 import '../../styles/font.scss'
 import React, { useState } from 'react'
 import FishAside from '../../components/FishAside'
-import { Link ,withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
-function OrderClass(props) {
+function OrderclassName(props) {
   return (
     <>
-      <div class="container h-100">
-        <div class="row">
-          <div class="col-12 bread-div">
-            <a class="clbread" href="*">
-              首頁 <span class="clspan">{'>'}</span>
+      <div className="container h-100">
+        <div className="row">
+          <div className="col-12 bread-div">
+            <a className="clbread" href="*">
+              首頁 <span className="clspan">{'>'}</span>
             </a>
-            <a class="clbread" href="*">
+            <a className="clbread" href="*">
               {' '}
-              會員中心 <span class="clspan">{'>'}</span>
+              會員中心 <span className="clspan">{'>'}</span>
             </a>
-            <a class="clbread" href="*">
+            <a className="clbread" href="*">
               {' '}
-              訂單資訊 <span class="clspan">{'>'}</span>
+              訂單資訊 <span className="clspan">{'>'}</span>
             </a>
-            <a class="clbread" href="*">
+            <a className="clbread" href="*">
               場地租借{' '}
             </a>
           </div>
         </div>
-        <div class="row justify-content-center">
-          <div class="col-2"></div>
-          <div class="col-9">
-            <h2 class="fish-topic">
-              <i class="fab fa-wpforms"></i> 場地租借
+        <div className="row justify-content-center">
+          <div className="col-2"></div>
+          <div className="col-9">
+            <h2 className="fish-topic">
+              <i className="fab fa-wpforms"></i> 場地租借
             </h2>
           </div>
         </div>
-        <div class="row justify-content-center">
+        <div className="row justify-content-center">
           <FishAside />
 
-          <div class="col-9 desk-order">
-            <div class="w-100 fish-order fish-order-pd d-flex justify-content-between">
-              <div class="col-2">
+          <div className="col-9 desk-order">
+            <div className="w-100 fish-order fish-order-pd d-flex justify-content-between">
+              <div className="col-2">
                 <h6>訂單編號</h6>
                 <a href="*">
                   {' '}
-                  <h6 class="fish-order-text">123456</h6>{' '}
+                  <h6 className="fish-order-text">123456</h6>{' '}
                 </a>
                 <a href="*">
                   {' '}
-                  <h6 class="fish-order-text">123456</h6>{' '}
+                  <h6 className="fish-order-text">123456</h6>{' '}
                 </a>
                 <a href="*">
                   {' '}
-                  <h6 class="fish-order-text">123456</h6>{' '}
+                  <h6 className="fish-order-text">123456</h6>{' '}
                 </a>
                 <a href="*">
                   {' '}
-                  <h6 class="fish-order-text">123456</h6>{' '}
+                  <h6 className="fish-order-text">123456</h6>{' '}
                 </a>
                 <a href="*">
                   {' '}
-                  <h6 class="fish-order-text">123456</h6>{' '}
+                  <h6 className="fish-order-text">123456</h6>{' '}
                 </a>
               </div>
-              <div class="col-2">
+              <div className="col-2">
                 <h6>租借館別</h6>
-                <h6 class="fish-order-text">大安館</h6>
-                <h6 class="fish-order-text">大安館</h6>
-                <h6 class="fish-order-text">大安館</h6>
-                <h6 class="fish-order-text">大安館</h6>
-                <h6 class="fish-order-text">大安館</h6>
+                <h6 className="fish-order-text">大安館</h6>
+                <h6 className="fish-order-text">大安館</h6>
+                <h6 className="fish-order-text">大安館</h6>
+                <h6 className="fish-order-text">大安館</h6>
+                <h6 className="fish-order-text">大安館</h6>
               </div>
-              <div class="col-2 dis-none">
+              <div className="col-2 dis-none">
                 <h6>租借日期</h6>
-                <h6 class="fish-order-text">2020/12/15</h6>
-                <h6 class="fish-order-text">2020/12/15</h6>
-                <h6 class="fish-order-text">2020/12/15</h6>
-                <h6 class="fish-order-text">2020/12/15</h6>
-                <h6 class="fish-order-text">2020/12/15</h6>
+                <h6 className="fish-order-text">2020/12/15</h6>
+                <h6 className="fish-order-text">2020/12/15</h6>
+                <h6 className="fish-order-text">2020/12/15</h6>
+                <h6 className="fish-order-text">2020/12/15</h6>
+                <h6 className="fish-order-text">2020/12/15</h6>
               </div>
-              <div class="col-2">
+              <div className="col-2">
                 <h6>租借時數</h6>
-                <h6 class="fish-order-text">2 hr</h6>
-                <h6 class="fish-order-text">2.5 hr</h6>
-                <h6 class="fish-order-text">3 hr</h6>
-                <h6 class="fish-order-text">2 hr</h6>
-                <h6 class="fish-order-text">2 hr</h6>
+                <h6 className="fish-order-text">2 hr</h6>
+                <h6 className="fish-order-text">2.5 hr</h6>
+                <h6 className="fish-order-text">3 hr</h6>
+                <h6 className="fish-order-text">2 hr</h6>
+                <h6 className="fish-order-text">2 hr</h6>
               </div>
 
-              <div class="col-2">
+              <div className="col-2">
                 <h6>訂單金額</h6>
-                <h6 class="fish-order-text">500</h6>
-                <h6 class="fish-order-text">1500</h6>
-                <h6 class="fish-order-text">1800</h6>
-                <h6 class="fish-order-text">180</h6>
-                <h6 class="fish-order-text">180</h6>
+                <h6 className="fish-order-text">500</h6>
+                <h6 className="fish-order-text">1500</h6>
+                <h6 className="fish-order-text">1800</h6>
+                <h6 className="fish-order-text">180</h6>
+                <h6 className="fish-order-text">180</h6>
               </div>
-              <div class="col-2">
+              <div className="col-2">
                 <h6>付款方式</h6>
-                <h6 class="fish-order-text">信用卡</h6>
-                <h6 class="fish-order-text">信用卡</h6>
-                <h6 class="fish-order-text">銀行轉帳</h6>
-                <h6 class="fish-order-text">銀行轉帳</h6>
-                <h6 class="fish-order-text">銀行轉帳</h6>
+                <h6 className="fish-order-text">信用卡</h6>
+                <h6 className="fish-order-text">信用卡</h6>
+                <h6 className="fish-order-text">銀行轉帳</h6>
+                <h6 className="fish-order-text">銀行轉帳</h6>
+                <h6 className="fish-order-text">銀行轉帳</h6>
               </div>
             </div>
-            <nav aria-label="Page navigation example" class="fish-order-mt">
-              <ul class="pagination justify-content-center">
-                <li class="page-item">
+            <nav aria-label="Page navigation example" className="fish-order-mt">
+              <ul className="pagination justify-content-center">
+                <li className="page-item">
                   <a
-                    class="page-link hoyu-page-link hoyu-page-arrow"
+                    className="page-link hoyu-page-link hoyu-page-arrow"
                     href="*"
                     aria-label="Previous"
                   >
                     <span aria-hidden="true">
-                      <i class="fas fa-chevron-left"></i>
+                      <i className="fas fa-chevron-left"></i>
                     </span>
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     1
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     2
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     3
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     4
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     5
                   </a>
                 </li>
-                <li class="page-item">
+                <li className="page-item">
                   <a
-                    class="page-link hoyu-page-link hoyu-page-arrow"
+                    className="page-link hoyu-page-link hoyu-page-arrow"
                     href="*"
                     aria-label="Next"
                   >
                     <span aria-hidden="true">
-                      <i class="fas fa-chevron-right"></i>
+                      <i className="fas fa-chevron-right"></i>
                     </span>
                   </a>
                 </li>
               </ul>
             </nav>
           </div>
-          <div class="col-8 mob-order">
-            <div class="mob-order-pd">
-              <div class="w-100 fish-order fish-order-pd d-flex">
-                <div class="col-6 mob-pd mob-topic">
+          <div className="col-8 mob-order">
+            <div className="mob-order-pd">
+              <div className="w-100 fish-order fish-order-pd d-flex">
+                <div className="col-6 mob-pd mob-topic">
                   <h6>訂單編號</h6>
                   <h6>租借館別</h6>
                   <h6>租借日期</h6>
@@ -167,7 +167,7 @@ function OrderClass(props) {
                   <h6>訂單金額</h6>
                   <h6>付款方式</h6>
                 </div>
-                <div class="col-6 mob-pd">
+                <div className="col-6 mob-pd">
                   <a href="*">
                     {' '}
                     <h6>123456</h6>{' '}
@@ -181,9 +181,9 @@ function OrderClass(props) {
                 </div>
               </div>
             </div>
-            <div class="mob-order-pd">
-              <div class="w-100 fish-order fish-order-pd d-flex">
-                <div class="col-6 mob-pd mob-topic">
+            <div className="mob-order-pd">
+              <div className="w-100 fish-order fish-order-pd d-flex">
+                <div className="col-6 mob-pd mob-topic">
                   <h6>訂單編號</h6>
                   <h6>租借館別</h6>
                   <h6>租借日期</h6>
@@ -191,7 +191,7 @@ function OrderClass(props) {
                   <h6>訂單金額</h6>
                   <h6>付款方式</h6>
                 </div>
-                <div class="col-6 mob-pd">
+                <div className="col-6 mob-pd">
                   <a href="*">
                     {' '}
                     <h6>123456</h6>{' '}
@@ -205,9 +205,9 @@ function OrderClass(props) {
                 </div>
               </div>
             </div>
-            <div class="mob-order-pd">
-              <div class="w-100 fish-order fish-order-pd d-flex">
-                <div class="col-6 mob-pd mob-topic">
+            <div className="mob-order-pd">
+              <div className="w-100 fish-order fish-order-pd d-flex">
+                <div className="col-6 mob-pd mob-topic">
                   <h6>訂單編號</h6>
                   <h6>租借館別</h6>
                   <h6>租借日期</h6>
@@ -215,7 +215,7 @@ function OrderClass(props) {
                   <h6>訂單金額</h6>
                   <h6>付款方式</h6>
                 </div>
-                <div class="col-6 mob-pd">
+                <div className="col-6 mob-pd">
                   <a href="*">
                     {' '}
                     <h6>123456</h6>{' '}
@@ -229,9 +229,9 @@ function OrderClass(props) {
                 </div>
               </div>
             </div>
-            <div class="mob-order-pd">
-              <div class="w-100 fish-order fish-order-pd d-flex">
-                <div class="col-6 mob-pd mob-topic">
+            <div className="mob-order-pd">
+              <div className="w-100 fish-order fish-order-pd d-flex">
+                <div className="col-6 mob-pd mob-topic">
                   <h6>訂單編號</h6>
                   <h6>租借館別</h6>
                   <h6>租借日期</h6>
@@ -239,7 +239,7 @@ function OrderClass(props) {
                   <h6>訂單金額</h6>
                   <h6>付款方式</h6>
                 </div>
-                <div class="col-6 mob-pd">
+                <div className="col-6 mob-pd">
                   <a href="*">
                     {' '}
                     <h6>123456</h6>{' '}
@@ -253,9 +253,9 @@ function OrderClass(props) {
                 </div>
               </div>
             </div>
-            <div class="mob-order-pd">
-              <div class="w-100 fish-order fish-order-pd d-flex">
-                <div class="col-6 mob-pd mob-topic">
+            <div className="mob-order-pd">
+              <div className="w-100 fish-order fish-order-pd d-flex">
+                <div className="col-6 mob-pd mob-topic">
                   <h6>訂單編號</h6>
                   <h6>租借館別</h6>
                   <h6>租借日期</h6>
@@ -263,7 +263,7 @@ function OrderClass(props) {
                   <h6>訂單金額</h6>
                   <h6>付款方式</h6>
                 </div>
-                <div class="col-6 mob-pd">
+                <div className="col-6 mob-pd">
                   <a href="*">
                     {' '}
                     <h6>123456</h6>{' '}
@@ -277,52 +277,52 @@ function OrderClass(props) {
                 </div>
               </div>
             </div>
-            <nav aria-label="Page navigation example" class="fish-order-mt">
-              <ul class="pagination justify-content-center">
-                <li class="page-item">
+            <nav aria-label="Page navigation example" className="fish-order-mt">
+              <ul className="pagination justify-content-center">
+                <li className="page-item">
                   <a
-                    class="page-link hoyu-page-link hoyu-page-arrow"
+                    className="page-link hoyu-page-link hoyu-page-arrow"
                     href="*"
                     aria-label="Previous"
                   >
                     <span aria-hidden="true">
-                      <i class="fas fa-chevron-left"></i>
+                      <i className="fas fa-chevron-left"></i>
                     </span>
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     1
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     2
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     3
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     4
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link hoyu-page-link" href="*">
+                <li className="page-item">
+                  <a className="page-link hoyu-page-link" href="*">
                     5
                   </a>
                 </li>
-                <li class="page-item">
+                <li className="page-item">
                   <a
-                    class="page-link hoyu-page-link hoyu-page-arrow"
+                    className="page-link hoyu-page-link hoyu-page-arrow"
                     href="*"
                     aria-label="Next"
                   >
                     <span aria-hidden="true">
-                      <i class="fas fa-chevron-right"></i>
+                      <i className="fas fa-chevron-right"></i>
                     </span>
                   </a>
                 </li>
@@ -331,9 +331,9 @@ function OrderClass(props) {
           </div>
         </div>
       </div>
-      <div class="bgup-img"></div>
-      <div class="bgdown-img"></div>
+      <div className="bgup-img"></div>
+      <div className="bgdown-img"></div>
     </>
   )
 }
-export default withRouter(OrderClass)
+export default withRouter(OrderclassName)
--- src/pages/member/register.js
+++ src/pages/member/register.js
@@ -1,20 +1,20 @@
 import '../../styles/fish.scss'
 import '../../styles/font.scss'
-import { Link ,withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 function register(props) {
   return (
     <>
-      <div class="fish-registerbg">
-        <div class="container h-100">
-          <div class="row justify-content-center">
-            <div class="fish-mask d-flex justify-content-center col-xl-6 col-lg-8 col-md-10 col-sm-12">
-              <form action="" class="pub-form w-80">
+      <div className="fish-registerbg">
+        <div className="container h-100">
+          <div className="row justify-content-center">
+            <div className="fish-mask d-flex justify-content-center col-xl-6 col-lg-8 col-md-10 col-sm-12">
+              <form action="" className="pub-form w-80">
                 <h3>會員註冊</h3>
                 <label for="account ">請輸入帳號</label>
                 <br />
                 <input
-                  class="w-100 member-input"
+                  className="w-100 member-input"
                   type="text"
                   name="acoount"
                   id="account"
@@ -25,7 +25,7 @@ function register(props) {
                 <label for="password">請輸入密碼</label>
                 <br />
                 <input
-                  class="w-100 member-input"
+                  className="w-100 member-input"
                   type="text"
                   name="password"
                   id="password"
@@ -35,7 +35,7 @@ function register(props) {
                 <label for="username">請輸入使用者名稱</label>
                 <br />
                 <input
-                  class="w-100 member-input"
+                  className="w-100 member-input"
                   type="text"
                   name="username"
                   id="username"
@@ -45,14 +45,14 @@ function register(props) {
                 <label for="email">請輸入電子郵件</label>
                 <br />
                 <input
-                  class="w-100 member-input"
+                  className="w-100 member-input"
                   type="text"
                   name="email"
                   id="email"
                 />
                 <small>請輸入正確電子郵件格式</small>
-                <div class="form-border w-100"></div>
-                <button type="button" class="pub-button mx-auto">
+                <div className="form-border w-100"></div>
+                <button type="button" className="pub-button mx-auto">
                   註冊
                 </button>
               </form>
