05307509cac7473665a1cfb4c45f0718692f1844
akirahasten <70085834+akirahasten@users.noreply.github.com>
2021-02-15 11:54:46 +0800

    新增分頁元件 pagination created

---
+++ src/components/Pagination.js
@@ -0,0 +1,65 @@
+import { Link } from 'react-router-dom'
+import { MdKeyboardArrowRight } from 'react-icons/md'
+import { MdKeyboardArrowLeft } from 'react-icons/md'
+import '../styles/hoyu.scss'
+// import '../../styles/font.scss'
+
+function pagination() {
+  return (
+    <>
+      <nav aria-label="Page navigation example" className="hoyu-mt">
+        <ul className="pagination justify-content-center">
+          <li className="page-item">
+            <Link
+              className="page-link hoyu-page-link hoyu-page-arrow"
+              to="#"
+              aria-label="Previous"
+            >
+              <span aria-hidden="true">
+                <MdKeyboardArrowLeft />
+              </span>
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link hoyu-page-link" to="#">
+              1
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link hoyu-page-link" to="#">
+              2
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link hoyu-page-link" to="#">
+              3
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link hoyu-page-link" to="#">
+              4
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link hoyu-page-link" to="#">
+              5
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link
+              className="page-link hoyu-page-link hoyu-page-arrow"
+              to="#"
+              aria-label="Next"
+            >
+              <span aria-hidden="true">
+                <MdKeyboardArrowRight />
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default pagination
