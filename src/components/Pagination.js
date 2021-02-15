8bcbdc730636f7b7a8c97ce0e31e521e83b1d32d
akirahasten <70085834+akirahasten@users.noreply.github.com>
2021-02-15 12:06:29 +0800

    分頁更新

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,6 +5,7 @@ import { MdKeyboardArrowLeft } from 'react-icons/md'
 function pagination() {
   return (
     <>
+      {/* 基本上就複製下面9-59行，中間頁數可以寫死，也可以用你自己迴圈產生的資料去判斷 */}
       <nav aria-label="Page navigation example" className="hoyu-mt">
         <ul className="pagination justify-content-center">
           <li className="page-item">
