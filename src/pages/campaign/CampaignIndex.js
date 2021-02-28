53be980a0d8e41e51e00d4950ed021fbebecb463
kaman6540fin <74940747+kaman6540fin@users.noreply.github.com>
2021-02-28 18:04:14 +0800

    加入動畫

--- src/pages/campaign/CampaignIndex.js
+++ src/pages/campaign/CampaignIndex.js
@@ -6,6 +6,8 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import DisplayCampaignCards from '../../components/DisplayCampaignCards'
 import indexVideo from '../../styles/img/indexVideo.mp4'
 import { useEffect } from 'react'
+import '../../styles/fontstyle.scss'
+import $ from 'jquery'
 
 function CampaignIndex(props) {
   const { searchCampaign, setSearchCampaign } = props
@@ -39,6 +41,15 @@ function CampaignIndex(props) {
     })
   }, [])
 
+  useEffect(() => {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 1000) {
+        $('.tastingContent').addClass('animate__fadeInLeft ')
+      } else {
+      }
+    })
+  }, [])
+
   return (
     <>
       <div class="cursorFinn d-none d-sm-block"></div>
@@ -59,7 +70,7 @@ function CampaignIndex(props) {
               最有質感的甜點活動
             </h2>
 
-            <p className="animate__animated animate__fadeIn animate__slow">
+            <p className="animate__animated animate__fadeIn animate__slow ">
               體驗獨一無二的活動體驗
             </p>
             <div
@@ -125,13 +136,14 @@ function CampaignIndex(props) {
         <Row className="h-100">
           <Col lg={7} className="tastingBg d-flex align-items-center">
             <Col lg={2}></Col>
-            <Col lg={5} className="tastingContent">
+            <Col lg={5} className="tastingContent animate__animated ">
               <h2 className="tastingTitle mb-4">甜點鑑賞</h2>
               <p className="tastingText">
-                將清新的烏龍茶，融入北海道的鮮奶油中，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，最後淋上海鹽奶油霜，沉穩而趣味的搭配。
-              </p>
-              <p className="tastingText">
-                將清新的烏龍茶，融入北海道澤鮮奶油中，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，最後淋上海鹽奶油霜，沉穩而趣味的搭配。
+                曾經被櫥窗裡的美麗甜點誘惑過的人，大概沒有不好奇它們是怎麼做出來的。
+                <br />
+                甜點製作是一門藝術，而藝術品從來不是只有吃下肚一途，在那之前藉著了解它的製作過程與細節，在品嘗時能更領略甜點師的用心、也更有樂趣。
+                <br />
+                醉糕品味，提供消費者從吃甜點進階到甜點鑑賞，希望能和更多朋友分享甜點藝術的奧妙。
               </p>
               <p
                 className="btn k_btn"
@@ -169,10 +181,11 @@ function CampaignIndex(props) {
             <Col lg={5} className="tastingContent">
               <h2 className="tastingTitle mb-4">手作體驗</h2>
               <p className="tastingText">
-                將清新的烏龍茶，融入北海道的鮮奶油中，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，最後淋上海鹽奶油霜，沉穩而趣味的搭配。
-              </p>
-              <p className="tastingText">
-                將清新的烏龍茶，融入北海道澤鮮奶油中，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，最後淋上海鹽奶油霜，沉穩而趣味的搭配。
+                手作課程，除了可以挑選自己喜愛的甜點、享受DIY的創作過程、還可以從中獲得滿滿的成就感。
+                <br />
+                不論是要送給心愛的朋友、家人、或是犒賞自己，都會是一種療癒身心的體驗唷！
+                <br />
+                醉糕品味提供最有趣甜點手作體驗，就算是完全新手，也能做出屬於自己的作品，滿足每一個喜歡甜點、熱愛手作、享受生活的你！
               </p>
               <p
                 className="btn k_btn"
@@ -204,10 +217,11 @@ function CampaignIndex(props) {
             <Col lg={5} className="tastingContent">
               <h2 className="tastingTitle mb-4">職人講座</h2>
               <p className="tastingText">
-                將清新的烏龍茶，融入北海道的鮮奶油中，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，最後淋上海鹽奶油霜，沉穩而趣味的搭配。
-              </p>
-              <p className="tastingText">
-                將清新的烏龍茶，融入北海道澤鮮奶油中，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，淡淡茶香搭配鬆軟的海綿蛋糕，加上雙層焦糖腰果，豐富了味蕾，最後淋上海鹽奶油霜，沉穩而趣味的搭配。
+                忙碌的生活總是需要心靈的調劑，創意生活的美學更是貼近人心，只有從心出發才會帶給生活美妙的體驗與經驗。
+                <br />
+                每個職人背後的人文故事及生活風格，往往才是引人入勝之處。醉糕品味認為成功的秘訣，必須從「專業」為出發點！
+                <br />
+                將專業與生活風格的結合已成為帶動消費者的行為趨勢，醉糕品味找來最具專業的職人，將體驗與美感成為您營造創意生活的基本元素。
               </p>
               <p
                 className="btn k_btn"
