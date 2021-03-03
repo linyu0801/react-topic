bbbecb2fc1123e8ff8b862ea3eb2f0ae58912225
Kris Tung <ragdoll.washoku89@gmail.com>
2021-03-03 15:16:17 +0800

    修改

--- src/components/CarouselShow.js
+++ src/components/CarouselShow.js
@@ -1,28 +1,53 @@
 import Carousel from 'react-elastic-carousel'
-import { Link, withRouter } from 'react-router-dom'
+import { Link, useHistory, withRouter } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function CarouselShow() {
+  const [product, setProduct] = useState([])
+  const history = useHistory()
+  useEffect(() => {
+    const FetchData = async () => {
+      const url = 'http://localhost:4000/mainproductcate' //讀取寫在node中的app.get('/mainproduct')
+      const request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify({ productCate: 1 }), //設定一進來畫面為全部商品的畫面
+        headers: new Headers({
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }),
+      })
+      const response = await fetch(request)
+      const rows = await response.json() //這邊的rows會得到所有資料庫中的資料
+      console.log('伺服器回傳看看', rows) //先在這邊console.log出rows得到的資料
+
+      setProduct(rows) //rows的東西會傳到product中，用setXXX的方式啟動整個畫面的更新
+    }
+
+    FetchData()
+  }, [])
   return (
     <>
       <div className="container">
         <Carousel itemsToScroll={3} itemsToShow={3} pagination={false}>
-          <div class="box">
-            <div className="img-box">
-              <Link to="#">
-                <img
-                  src="http://localhost:3000/k-images/asset 33.jpeg"
-                  class="card-img-top product-img"
-                  alt="cake29"
-                />
-              </Link>
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">花生好事</h5>
+          {product.map((v, i) => (
+            <div class="box" key={i}>
+              <div className="img-box">
+                <Link to={`/mainproductdetail/products/` + v.p_sid}>
+                  <img
+                    src={`http://localhost:3000/k-images/${v.p_img}`}
+                    class="card-img-top product-img"
+                    alt="cake29"
+                  />
+                </Link>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">{v.p_name}</h5>
 
-              <div class="product-price">$ 650</div>
+                <div class="product-price">$ {v.p_price}</div>
+              </div>
             </div>
-          </div>
-          <div className="box">
+          ))}
+          {/* <div className="box">
             <div class="img-box">
               <Link to="#">
                 <img
@@ -85,7 +110,7 @@ function CarouselShow() {
 
               <div class="product-price">$ 650</div>
             </div>
-          </div>
+          </div> */}
         </Carousel>
       </div>
     </>
